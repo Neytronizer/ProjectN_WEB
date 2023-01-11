759ee4b23650d4f6851b922bc825ff416e150566
Fabrix <swizls12@gmail.com>
2023-01-12 01:10:51 +0200

    Добавлен модальное окно логина

--- WEB/app.py
+++ WEB/app.py
@@ -1,8 +1,7 @@
-from enum import unique
-from os import name
 from flask import Flask, render_template, request, redirect, url_for
 from flask.sessions import NullSession
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
@@ -19,6 +18,7 @@ class Users(db.Model):
     login = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(20), unique=False, nullable=False)
     nickname = db.Column(db.String(20), unique=False, nullable=False)
+    registrationDate = db.Column(db.DateTime, unique=False, nullable=False, default=datetime.utcnow)
 
 class Coments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,113 +29,19 @@ class Coments(db.Model):
         return '<User %r>' % self.username
 
 
-# @app.route('/', methods=['POST', 'GET'])
-# @app.route('/<string:lang>', methods=['POST', 'GET'])
-# def mainPage(lang="langEn"):
-#     admin = Users.query.first()
-#     if request.method == "POST":
-#         user = request.form['user']
-#         password = request.form['password']
-#         if admin.username == user and admin.password == password:
-#             return redirect('/admin_panel')
-#         else:
-#             return redirect('/')
-#     else:
-#         if Cruises.query.first() != None:
-#             offers = Cruises.query.all()
-#             if(lang == "langEn"):
-#                 print("En")
-#                 return render_template('main_page.html', offers=offers)
-#             elif(lang == "langLv"):
-#                 print("Lv")
-#                 return render_template('main_page_lv.html', offers=offers)
-#             else:
-#                 print("Ru")
-#                 return render_template('main_page_ru.html', offers=offers)
-#         else:
-#             if(lang == "langEn"):
-#                 print("En")
-#                 return render_template('main_page.html')
-#             elif(lang == "langLv"):
-#                 print("Lv")
-#                 return render_template('main_page_lv.html')
-#             else:
-#                 print("Ru")
-#                 return render_template('main_page_ru.html')
-
-# @app.route('/admin_panel', methods=['POST', 'GET'])
-# def adminPanel():
-#     if request.method == "POST":
-#         if request.form['selectType'] == "Port":
-#             portName = request.form['portName']
-
-#             port = Ports(name=portName)
-#             try:
-#                 db.session.add(port)
-#                 db.session.commit()
-#                 return redirect('/admin_panel')
-#             except:
-#                 return "ошибка"
-#         elif request.form['selectType'] == "Ship":
-#             ship = request.form['shipText']
-#             passengerCapacity = request.form['shipCapacity']
-
-#             ships = Ships(ship=ship, passengerCapacity=passengerCapacity)
-#             try:
-#                 db.session.add(ships)
-#                 db.session.commit()
-#                 return redirect('/admin_panel')
-#             except:
-#                 return "ошибка"
-#         elif request.form['selectType'] == "Cruise":
-#             ship = request.form['shipSelect']
-#             fromPort = request.form['from']
-#             toPort = request.form['to']
-#             departDate = request.form['departDate']
-#             price = request.form['price']
-
-#             cruise = Cruises(ship=ship, departDate=departDate, floatsFrom=fromPort, floatsTo=toPort, price=price)
-#             try:
-#                 db.session.add(cruise)
-#                 db.session.commit()
-#                 return redirect('/admin_panel')
-#             except:
-#                 return "ошибка"
-#     else:
-#         if Cruises.query.first() != None:
-#             offers = Cruises.query.all()
-#             ships = Ships.query.all()
-#             ports = Ports.query.all()
-#             return render_template('admin_panel.html', offers=offers, ships=ships, ports=ports)
-#         else:
-#             return render_template('admin_panel.html')
-
-# @app.route('/admin_panel_<string:type>_<int:id>', methods=['POST', 'GET'])
-# def admin_panel_delete(type, id):
-#             if type == "port":
-#                 port = Ports.query.get_or_404(id)
-#                 try:
-#                     db.session.delete(port)
-#                     db.session.commit()
-#                     return redirect('/admin_panel')
-#                 except:
-#                     return "error"
-#             if type == "ship":
-#                 ship = Ships.query.get_or_404(id)
-#                 try:
-#                     db.session.delete(ship)
-#                     db.session.commit()
-#                     return redirect('/admin_panel')
-#                 except:
-#                     return "error"
-#             if type == "cruise":
-#                 cruise = Cruises.query.get_or_404(id)
-#                 try:
-#                     db.session.delete(cruise)
-#                     db.session.commit()
-#                     return redirect('/admin_panel')
-#                 except:
-#                     return "error"
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/<int:userID>', methods=['POST', 'GET'])
+def mainPage(userID=0):
+    if request.method == "POST":
+        users = Users.query.first()
+        user = request.form['login']
+        password = request.form['password']
+        if users.login == user and users.password == password:
+            return redirect('/admin_panel')
+        else:
+            return redirect('/')
+    else:
+        return render_template('index.html')
     
-# if __name__ == "__main__":
-#     app.run(debug=True, use_reloader=False, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False, host='0.0.0.0')
